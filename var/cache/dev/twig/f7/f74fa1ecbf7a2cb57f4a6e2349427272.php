76170aac20eb909039511ac327262aab049e7c6c
Meriem Ben Hamida <Meriem.benhamisa@esprit.tn>
2024-02-12 16:07:52 +0100

    first

---
+++ var/cache/dev/twig/f7/f74fa1ecbf7a2cb57f4a6e2349427272.php
@@ -0,0 +1,216 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* backend/Breeze-Free-Bootstrap-Admin-Template-master/README.md */
+class __TwigTemplate_4434e425c891fa3548a02e9b2be3be75 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "backend/Breeze-Free-Bootstrap-Admin-Template-master/README.md"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "backend/Breeze-Free-Bootstrap-Admin-Template-master/README.md"));
+
+        // line 1
+        echo "<table>
+<tr>
+<td>
+<a href=\"https://www.bootstrapdash.com/demo/breeze-free/template/index.html\" target=\"_blank\"><img src=\"screenshot.jpg\"></a>
+</td>
+</tr>
+</table>
+
+<h1>BreezeAdmin-Free-Admin-Template</h1>
+Breeze Admin is a free responsive admin template built with Bootstrap 4. The template has colorful, attractive yet simple and elegant design. The template is well crafted, with all the components neatly and carefully designed and arranged within the template.
+
+Breeze Admin is packed with all the features that fit your needs but not cramped with components you would not even use. It is an excellent fit to build admin panels, e-commerce systems, project management systems, CMS or CRM.
+
+Although the template has a design like none other, it is easily customizable to suit your requirements. Breeze Admin comes with a clean and well-commented code that makes it easy to work with the template. Thus making it an ideal pick for jump starting your project.
+
+<h1>Credits:</h1>
+
+- Bootstrap 4
+- Material Design Icons
+- jQuery
+- Gulp
+- Chart.js
+
+<h1>Browser Support:</h1>
+
+Breeze Admin is designed to work flawlessly with all the latest and modern web browsers.
+
+- Chrome (latest)
+- FireFox (latest)
+- Safari (latest)
+- Opera (latest)
+- IE10+
+
+<h1>License Information:</h1>
+
+Breeze Admin is released under MIT license. Breeze Admin is a free Bootstrap 4 admin template developed from BootstrapDash. Feel free to download it, use it, share it, get creative with it.
+
+<h1>How to use Breeze Admin?</h1>
+
+1 - Click the Clone or Download button in GitHub and download as a ZIP file or you can enter the command git clone https://github.com/BootstrapDash/Breeze-Free-Bootstrap-Admin-Template.git in you terminal to get a copy of this template.
+
+2 - After the files have been downloaded you will get a folder with all the required files
+
+3 - You can install all the dependencies in the template by running the command npm install. All the required files are in the node modules.
+
+4 - Find the file named index.html, check what all components you need. Open the file in a text editor and you can start editing.
+
+5 - Now that your project has now kick-started, all you need to do now is to code, code, and code to your heart's content.
+
+<h1>How to Contribute?:</h1>
+
+We love your contributions and we welcome them wholeheartedly. We believe the more the merrier.
+To contribute make sure you have a Node.js and npm installed. Now run the command gulp --version. If the command returns with the Gulp version number, it means you have Gulp installed. If not you need to run the command npm install --global gulp-cli to install Gulp.
+
+<h2>Next</h2>
+
+After Gulp has been installed, follow the steps below to contribute.
+<br>
+1 - Fork and clone the repo of Breeze Admin.
+<br>
+2 - Run the command npm install to install all the dependencies.
+<br>
+3 - Enter the command gulp serve. This will open Breeze Admin in your default browser.
+<br>
+4 - Make your valuable contribution
+<br>
+5 - Submit a pull request.
+
+  <hr>
+\tDo you need a template with more features and functionalities? Get more with our collection of the premium template with more plugins, eye catching animations, UI components, and sample pages all fitting together with a high-quality design.
+Visit
+  <a href=\"https://www.bootstrapdash.com\" target=\"_blank\">https://www.bootstrapdash.com</a> for more admin templates.
+";
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName()
+    {
+        return "backend/Breeze-Free-Bootstrap-Admin-Template-master/README.md";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo()
+    {
+        return array (  43 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("<table>
+<tr>
+<td>
+<a href=\"https://www.bootstrapdash.com/demo/breeze-free/template/index.html\" target=\"_blank\"><img src=\"screenshot.jpg\"></a>
+</td>
+</tr>
+</table>
+
+<h1>BreezeAdmin-Free-Admin-Template</h1>
+Breeze Admin is a free responsive admin template built with Bootstrap 4. The template has colorful, attractive yet simple and elegant design. The template is well crafted, with all the components neatly and carefully designed and arranged within the template.
+
+Breeze Admin is packed with all the features that fit your needs but not cramped with components you would not even use. It is an excellent fit to build admin panels, e-commerce systems, project management systems, CMS or CRM.
+
+Although the template has a design like none other, it is easily customizable to suit your requirements. Breeze Admin comes with a clean and well-commented code that makes it easy to work with the template. Thus making it an ideal pick for jump starting your project.
+
+<h1>Credits:</h1>
+
+- Bootstrap 4
+- Material Design Icons
+- jQuery
+- Gulp
+- Chart.js
+
+<h1>Browser Support:</h1>
+
+Breeze Admin is designed to work flawlessly with all the latest and modern web browsers.
+
+- Chrome (latest)
+- FireFox (latest)
+- Safari (latest)
+- Opera (latest)
+- IE10+
+
+<h1>License Information:</h1>
+
+Breeze Admin is released under MIT license. Breeze Admin is a free Bootstrap 4 admin template developed from BootstrapDash. Feel free to download it, use it, share it, get creative with it.
+
+<h1>How to use Breeze Admin?</h1>
+
+1 - Click the Clone or Download button in GitHub and download as a ZIP file or you can enter the command git clone https://github.com/BootstrapDash/Breeze-Free-Bootstrap-Admin-Template.git in you terminal to get a copy of this template.
+
+2 - After the files have been downloaded you will get a folder with all the required files
+
+3 - You can install all the dependencies in the template by running the command npm install. All the required files are in the node modules.
+
+4 - Find the file named index.html, check what all components you need. Open the file in a text editor and you can start editing.
+
+5 - Now that your project has now kick-started, all you need to do now is to code, code, and code to your heart's content.
+
+<h1>How to Contribute?:</h1>
+
+We love your contributions and we welcome them wholeheartedly. We believe the more the merrier.
+To contribute make sure you have a Node.js and npm installed. Now run the command gulp --version. If the command returns with the Gulp version number, it means you have Gulp installed. If not you need to run the command npm install --global gulp-cli to install Gulp.
+
+<h2>Next</h2>
+
+After Gulp has been installed, follow the steps below to contribute.
+<br>
+1 - Fork and clone the repo of Breeze Admin.
+<br>
+2 - Run the command npm install to install all the dependencies.
+<br>
+3 - Enter the command gulp serve. This will open Breeze Admin in your default browser.
+<br>
+4 - Make your valuable contribution
+<br>
+5 - Submit a pull request.
+
+  <hr>
+\tDo you need a template with more features and functionalities? Get more with our collection of the premium template with more plugins, eye catching animations, UI components, and sample pages all fitting together with a high-quality design.
+Visit
+  <a href=\"https://www.bootstrapdash.com\" target=\"_blank\">https://www.bootstrapdash.com</a> for more admin templates.
+", "backend/Breeze-Free-Bootstrap-Admin-Template-master/README.md", "C:\\Users\\Meriem\\Desktop\\hermesvoyages\\templates\\backend\\Breeze-Free-Bootstrap-Admin-Template-master\\README.md");
+    }
+}
